8b936516a05378da831945be170f3841b7bf1318
shortcutmaniac <101396582+shortcutmaniac@users.noreply.github.com>
2023-06-20 14:32:03 +0200

    Update gitfinder.py

--- gitfinder.py
+++ gitfinder.py
@@ -9,21 +9,24 @@ import encodings.idna
 import http.client
 
 
-def findgitrepo(output_file, domains):
+def findgitrepo(output_file, domain_with_port):
+    domain, port = domain_with_port.split(':')
+    port = int(port)
+    
     try:
-        domain = ".".join(encodings.idna.ToASCII(label).decode("ascii") for label in domains.strip().split("."))
+        domain = ".".join(encodings.idna.ToASCII(label).decode("ascii") for label in domain.strip().split("."))
     except (UnicodeError, ValueError):
         return
 
     try:
-        # Try to download http://target.tld/.git/HEAD
-        with urlopen(''.join(['http://', domain, '/.git/HEAD']), context=ssl._create_unverified_context(), timeout=5) as response:
+        # Try to download https://target.tld:8443/.git/HEAD
+        with urlopen(''.join(['https://', domain, ':', str(port), '/.git/HEAD']), context=ssl._create_unverified_context(), timeout=5) as response:
             answer = response.read(200).decode('utf-8', 'ignore')
 
     except (HTTPError, URLError, OSError, ConnectionResetError, ValueError):
         return
     except http.client.BadStatusLine:
-        print(f"BadStatusLine error for URL: http://{domain}/.git/HEAD")
+        print(f"BadStatusLine error for URL: https://{domain}:{port}/.git/HEAD")
         return
     except (KeyboardInterrupt, SystemExit):
         raise
@@ -41,7 +44,7 @@ def findgitrepo(output_file, domains):
 
 def read_file(filename):
     with open(filename) as file:
-        return file.readlines()
+        return [line.strip() for line in file.readlines()]
 
 
 def main():
@@ -71,14 +74,14 @@ def main():
         sys.exit(err)
 
     try:
-        domains = read_file(domain_file)
+        domains_with_ports = read_file(domain_file)
     except FileNotFoundError as err:
         sys.exit(err)
 
     fun = partial(findgitrepo, output_file)
     print("Scanning...")
     with Pool(processes=max_processes) as pool:
-        pool.map(fun, domains)
+        pool.map(fun, domains_with_ports)
     print("Finished")
 
 
